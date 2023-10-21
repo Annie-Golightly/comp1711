ff5463264391de7f13b37dd6830de240fe8339d2
Annie-Golightly <146282648+Annie-Golightly@users.noreply.github.com>
2023-10-21 02:51:38 +0000

    Worksheet 1.2

--- C_Bootcamp_Worksheet_2/ex3.c
+++ C_Bootcamp_Worksheet_2/ex3.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
 int main(){
+    int numbers[5] = {1,2,3,4,5};
+    int highest = 0;
+    int i;
+    for (i=0; i<5; i++){
+        if (numbers[i] > highest){
+            highest = numbers[i];
+        }
+        else{
+        }
+    }
+    printf("%d\n",highest);
+    return 0;
+
+    
     
 }
